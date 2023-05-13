067e46495573baca292d8e4e595dd0cfb73ca025
Abhishek Gokavarapu <iamabhishekgokavarapu@gmail.com>
2023-05-13 21:05:27 +0530

    Implemented Card and AppBar Theme in theme manager

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced/presentation/color_manager.dart';
+import 'package:flutter_advanced/presentation/font_manager.dart';
+import 'package:flutter_advanced/presentation/styles_manager.dart';
+import 'package:flutter_advanced/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
@@ -9,11 +12,21 @@ ThemeData getApplicationTheme() {
       primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager
           .grey1, // will be used incase of disabled button for example
-      hintColor: ColorManager.grey
+      hintColor: ColorManager.grey,
       // card view theme
+      cardTheme: CardTheme(
+          color: ColorManager.white,
+          shadowColor: ColorManager.grey,
+          elevation: AppSize.s4),
 
       // App bar theme
-
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.primary,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.primaryOpacity70,
+          titleTextStyle: getRegularStyle(
+              color: ColorManager.white, fontSize: FontSize.s16))
       // Button theme
 
       // Text theme
