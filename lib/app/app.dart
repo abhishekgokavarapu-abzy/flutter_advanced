6e48100a0c807cad8cd71c532eee09370b1f63b0
Abhishek Gokavarapu <iamabhishekgokavarapu@gmail.com>
2023-05-14 21:35:37 +0530

    Implemented Splash Screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced/presentation/resources/routes_manager.dart';
 import 'package:flutter_advanced/presentation/resources/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
@@ -16,12 +17,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
     );
   }
 }
